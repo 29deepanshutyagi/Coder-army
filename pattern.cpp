d169e1bc2ae3dcc095240e6be5508510f3cc73a6
29deepanshutyagi <123951725+29deepanshutyagi@users.noreply.github.com>
2023-09-30 15:47:59 +0530

    pattern 1

---
+++ pattern.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int row,col;
+    // for(row=1;row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+// int row,col;
+//   for(row=1;row<=4;row++){
+//     for(col=1;col<=5;col++){
+//         cout<<"10 ";
+//     }
+//     cout<<endl;
+//   }
+//  int row,col;
+//   for(row=1;row<=5;row++){
+//     for(col=1;col<=5;col++){
+//         cout<<col<<" ";
+//     }
+//     cout<<endl;
+//   }ṇ
+
+
+//  int row,col;
+//    for(row=1;row<=5;row++){
+//     for(col=5;col>=1;col--){
+//         cout<<col<<" ";
+//     }
+    // cout<<endl;
+//    }
+
+// int row,col;
+// for(row=1;row<=5;row++){
+//     for(col=1;col<=5;col++){
+//         cout<<col*col<<" ";
+//     }
+//     cout<<endl;
+// }
+// int row,col;
+// char m='a';
+// for(row=1;row<=5;row++){
+//     for(col=1;col<=5;col++){
+//         cout<<m<<" ";
+//     }
+//     m=m+1;
+//     cout<<endl;
+// int row,col;
+// char m='a';
+// for(row=1;row<=5;row++){
+//     m='a';
+//     for(col=1;col<=5;col++){
+
+//         cout<<m<<" ";
+//         m=m+1;
+//     }
+   
+//     cout<<endl;
+// }
+
+int row,col,k=1;
+ for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+        cout<<(row-1)*5+col<<" ";
+    }
+    cout<<endl;
+
+  }
+
+
+}
+
